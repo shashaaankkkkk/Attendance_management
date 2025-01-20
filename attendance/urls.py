7e270d8e86767064f828f6c7e2daf02263afe7b0
shashank <shashankshekhar8534@gmail.com>
2025-01-20 07:55:32 +0530

    added changes

--- attendance/urls.py
+++ attendance/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
+    path('', views.user_login, name='login'),
     path('teacher/dashboard/', views.teacher_dashboard, name='teacher_dashboard'),
     path('class/<int:class_id>/mark/', views.mark_attendance, name='mark_attendance'),
     path('class/<int:class_id>/attendance/', views.show_attendance, name='show_attendance'),
