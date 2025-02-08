a5e1446887fa19e6a842cd009b215198e0d88cf8
shashank <shashankshekhar8534@gmail.com>
2025-02-09 01:38:02 +0530

    some fix

--- attendance_app/settings.py
+++ attendance_app/settings.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
+from dotenv import load_dotenv
+# Load environment variables from .env file
+load_dotenv()
 
 from pathlib import Path
 
@@ -128,9 +132,9 @@ AUTH_USER_MODEL = 'attendance.User'
 
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # Or your email provider's SMTP server
+EMAIL_HOST = os.getenv("EMAIL_HOST", default="") # Or your email provider's SMTP server
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'shashankshekhar8534@gmail.com'
-EMAIL_HOST_PASSWORD = 'juoq jmnl gybb atbf'
-DEFAULT_FROM_EMAIL = 'shashankshekhar8534@gmail.com'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", default="")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", default="")
